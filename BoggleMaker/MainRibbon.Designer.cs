7bd571819b935e43ea984a8f8483eeef6d29b589
dtylam <dlamty@gmail.com>
2016-10-07 17:02:10 +0100

    random first version

---
+++ BoggleMaker/MainRibbon.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace BoggleMaker {
+    partial class MainRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public MainRibbon()
+            : base(Globals.Factory.GetRibbonFactory()) {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainRibbon));
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.MainRibbonGrp = this.Factory.CreateRibbonGroup();
+            this.InsertNew4 = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.MainRibbonGrp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.MainRibbonGrp);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // MainRibbonGrp
+            // 
+            this.MainRibbonGrp.Items.Add(this.InsertNew4);
+            this.MainRibbonGrp.Label = "BoggleMaker";
+            this.MainRibbonGrp.Name = "MainRibbonGrp";
+            // 
+            // InsertNew4
+            // 
+            this.InsertNew4.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.InsertNew4.Image = ((System.Drawing.Image)(resources.GetObject("InsertNew4.Image")));
+            this.InsertNew4.ImageName = "grid4";
+            this.InsertNew4.Label = "New 4x4 Boggle";
+            this.InsertNew4.Name = "InsertNew4";
+            this.InsertNew4.ShowImage = true;
+            this.InsertNew4.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.InsertNew4_Click);
+            // 
+            // MainRibbon
+            // 
+            this.Name = "MainRibbon";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.MainRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.MainRibbonGrp.ResumeLayout(false);
+            this.MainRibbonGrp.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup MainRibbonGrp;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton InsertNew4;
+    }
+
+    partial class ThisRibbonCollection {
+        internal MainRibbon Ribbon1 {
+            get { return this.GetRibbon<MainRibbon>(); }
+        }
+    }
+}
---
+++ BoggleMaker/MainRibbon.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BoggleMaker.Properties;
+using Microsoft.Office.Interop.Word;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace BoggleMaker {
+    public partial class MainRibbon {
+        private static Random rand = new Random();
+        private void MainRibbon_Load(object sender, RibbonUIEventArgs e) {
+
+        }
+
+        private void InsertNew4_Click(object sender, RibbonControlEventArgs e) {
+            Document document = Globals.ThisAddIn.Application.ActiveDocument;
+            //typing header
+            Selection currentSelection = Globals.ThisAddIn.Application.Selection;
+            currentSelection.TypeText("R&D IT's");
+            try {
+                var filePath = @"Resources\boggle_logo.png";
+                currentSelection.InlineShapes.AddPicture(filePath);
+            }
+            catch (Exception ex) {
+                currentSelection.TypeText(" Boggle");
+            }
+            //typing rules
+            currentSelection.TypeText("\n\n1. Words must be 3+ letters\n\n"+
+                                      "2. Words cannot be: names, hyphenated or abbreviated.\n\n" +
+                                      "3. Write your words (linked or separated) in the table below:\n\n");
+            //inserting table
+            currentSelection = Globals.ThisAddIn.Application.Selection;
+            document.Tables.Add(currentSelection.Range, 4, 4);
+            Tables tables = document.Tables;
+            Table boggleTable = tables[tables.Count];
+            #region boggleTable styling
+            boggleTable.Borders.InsideLineStyle = WdLineStyle.wdLineStyleSingle;
+            boggleTable.Borders.OutsideLineStyle = WdLineStyle.wdLineStyleSingle;
+            boggleTable.Range.Font.Size = 36;
+            boggleTable.Range.Font.Bold = 1;
+            boggleTable.Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+            #endregion
+            FillTableWithLetters(boggleTable);
+            
+        }
+
+        private void FillTableWithLetters(Table boggleTable) {
+            int maxCol = boggleTable.Columns.Count;
+            int maxRow = boggleTable.Rows.Count;
+            for (int col = 1; col <= maxCol; col++) {
+                for (int row = 1; row <= maxRow; row++) {
+                    Cell cell = boggleTable.Cell(row, col);
+                    cell.Range.Text = GenRandomLetter();
+                    cell.VerticalAlignment = WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+                    if (row % 2 == 0 && col % 2 == 0)
+                        cell.Range.Shading.BackgroundPatternColor = WdColor.wdColorGray20;
+                    if (row % 2 == 1 && col % 2 == 1)
+                        cell.Range.Shading.BackgroundPatternColor = WdColor.wdColorGray15;
+                    cell.SetWidth(60, WdRulerStyle.wdAdjustNone);
+                }
+            }
+        }
+
+        private string GenRandomLetter() {
+            string[] consonants = {
+                "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N",
+                "P", "Qu", "R", "S", "T", "V", "W", "X", "Y", "Z"
+            };
+            string[] vowels = {
+                "A", "E", "I", "O", "U"
+            };
+            int chances = rand.Next(5);
+            switch (chances) {
+                case 1: case 2:
+                    return vowels[rand.Next(5)];
+                default:
+                    return consonants[rand.Next(21)];
+            }
+        }
+    }
+}
